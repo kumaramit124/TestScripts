7a407216597a422411b8ac3f44483e6b0753f1a9
Amit Kumar <kumaramit124@gmail.com>
2021-06-04 08:15:41 +0530

    Fix the whitespace

--- C/duplicateRemove.c
+++ C/duplicateRemove.c
@@ -25,19 +25,17 @@ int main()
 {
     int n = 0;
     int i;
-    int arr[] = {2,4,5,2,4,6,7,4};
+    int arr[] = {2,4,5,3,2,4,6,7,4};
     int arr1[] = {0};
     int arr_size = sizeof(arr)/sizeof(arr[0]);
-    printf("the size arr: %d\n", arr_size);
-
-    // removeDuplicates() returns new size of
-    // array.
+    printf("the size of arr: %d\n", arr_size);
+    
+    // removeDuplicates() returns new size of array.
     n = removeDuplicates(arr, arr_size);
     printf("duplicate: %d\n", n);
     for(i=0; i<n; i++)
     {
-         printf("Arr: %d\n", arr[i]);
+	    printf("Arr: %d\n", arr[i]);
     }
-
     return 0;
 }
